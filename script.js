6d34e713af94e9ebb3d675159c7b21169ada529c
Caleb Lombard <caleblombard58@gmail.com>
2023-10-10 18:09:29 +0200

    Added Files To GitHub - Finished Etch-A-Sketch Program (Fully Functional)

--- script.js
+++ script.js
@@ -21,15 +21,14 @@ const createGrid = (amountOfGrids) => {
         for (let j=0; j < amountOfGrids; j++) {
             const { r, g, b} = createRandomRGB ()
 
-            const widthAndHeight = 960 / amountOfGrids
+            const widthAndHeight = 600 / amountOfGrids
             const gridBox = document.createElement("div")
             gridBox.classList.add("grid-box")
             gridBox.style.width = `${widthAndHeight}px`
             gridBox.style.height = `${widthAndHeight}px`
 
             gridBox.addEventListener("mouseenter", () => {
-                const bgColor = "rgb(" + r + "," + g + "," + b + ")";
-                gridBox.style.background = bgColor
+                gridBox.style.background = "black"
             })
             row.appendChild(gridBox)
         }
@@ -53,3 +52,89 @@ resetButton.addEventListener("click", () => {
     createGrid(userSize)
 })
 
+// Add this code below your existing JavaScript code
+
+const changeColorButton = document.querySelector(".change-color");
+let isChangingColor = false;
+
+changeColorButton.addEventListener("click", () => {
+    isChangingColor = !isChangingColor;
+
+    if (isChangingColor) {
+        changeColorButton.textContent = "Stop Rainbow Mode";
+        startChangingColor();
+    } else {
+        changeColorButton.textContent = "Change Color";
+        stopChangingColor();
+    }
+});
+
+function startChangingColor() {
+    const gridBoxes = document.querySelectorAll(".grid-box");
+    
+    gridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener("mouseenter", changeColorOnHover);
+    });
+}
+
+function stopChangingColor() {
+    const gridBoxes = document.querySelectorAll(".grid-box");
+    
+    gridBoxes.forEach((gridBox) => {
+        gridBox.removeEventListener("mouseenter", changeColorOnHover);
+    });
+}
+
+function changeColorOnHover() {
+    const { r, g, b } = createRandomRGB();
+    const bgColor = "rgb(" + r + "," + g + "," + b + ")";
+    this.style.background = bgColor;
+}
+
+// Add this code below your existing JavaScript code
+
+const addTintButton = document.querySelector(".add-tint");
+let isTinting = false;
+
+addTintButton.addEventListener("click", () => {
+    isTinting = !isTinting;
+
+    if (isTinting) {
+        addTintButton.textContent = "Remove Black & White";
+        addRandomTint();
+    } else {
+        addTintButton.textContent = "Add Black & White";
+        removeTint();
+    }
+});
+
+function addRandomTint() {
+    const gridBoxes = document.querySelectorAll(".grid-box");
+    
+    gridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener("mouseenter", addRandomTintOnHover);
+    });
+}
+
+function addRandomTintOnHover() {
+    const currentColor = this.style.backgroundColor;
+    if (!currentColor) return;
+
+    const rgbValues = currentColor.match(/\d+/g);
+    if (rgbValues.length !== 3) return;
+
+    const r = Math.min(255, parseInt(rgbValues[0]) + Math.random() * 25.5);
+    const g = Math.min(255, parseInt(rgbValues[1]) + Math.random() * 25.5);
+    const b = Math.min(255, parseInt(rgbValues[2]) + Math.random() * 25.5);
+
+    const tintedColor = `rgb(${r},${g},${b})`;
+    this.style.backgroundColor = tintedColor;
+}
+
+function removeTint() {
+    const gridBoxes = document.querySelectorAll(".grid-box");
+    
+    gridBoxes.forEach((gridBox) => {
+        gridBox.removeEventListener("mouseenter", addRandomTintOnHover);
+    });
+}
